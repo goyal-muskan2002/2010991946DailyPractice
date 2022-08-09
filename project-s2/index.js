a7ecfa256bd8a719332b582296ea51733cbf1a8a
unknown <muskan1946.be20@chitkara.edu.in>
2022-08-09 13:50:04 +0530

    added file

--- project-s2/index.js
+++ project-s2/index.js
@@ -1,11 +1,27 @@
-function enter( ){
-    var n= document.getElementById("number").value;
-    n=Number(n);
-    if(n.value>5){
-       alert("enter a number which is of only 5 digits");
+function fnClick(){
+           
+    var  n=document.getElementById('number').value;
+    n=(Number)(n);
+    var sum=0;
+    var count=0;
+    while(n>0){
+        rem=n%10;
+        sum=sum+rem;
+        n=Math.floor(n/10);
+        count++;
+    }
+    if(count>5 || count<5){
+        alert("Please enter only 5 numbers");
     }
     else{
-        return true;
+        var radius=(Math.floor(sum/5));
     }
+
+    document.getElementById('p').style.height=sum+"px";
+    document.getElementById('p').style.width=sum+"px";
+    document.getElementById('p').style.backgroundColor="red";
+    document.getElementById('p').style.transition="all 4s";
+    document.getElementById('p').style.animation="my 4s";
+    document.getElementById('p').style.borderRadius=radius+"px";
 
 }
