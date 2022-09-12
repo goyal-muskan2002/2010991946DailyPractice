b229e347bdf54a46a91517d261dd20ebb82495fe
unknown <muskan1946.be20@chitkara.edu.in>
2022-09-12 13:43:39 +0530

    st3project

---
+++ 2.14july2022/script.js
@@ -0,0 +1,35 @@
+// console.warn("this is an error");
+var num1= 10;
+var num2= 20;
+console.log(num1+num2);
+var str1=20;
+var str2="this is muskn goyal";
+var str3="this is intelligent";
+console.log(str1+str2+str3);
+var und;
+console.log(und);
+var arr=[1,2,"string", 3,4];
+console.log(arr);
+function avg(a,b ){
+    return (a+b)/2;
+}
+var c= avg(2,5);
+var c1= avg(5,6);
+console.log(c);
+console.log(c1);
+var age=4;
+if(age>4){
+    console.log("you are a childe");
+}
+else{
+    console.log("you are a  niot childe");
+}
+ var arr=[1,2,3,4,5,6];
+ arr.forEach(function(element)
+ {
+    console.log(element);
+ });
+ var arr1=[5,6,7,8,9];
+ for(var i=0; i<arr1.length; i++){
+    console.log(arr1[i]);
+ }
